55b12291dacf235295358a7e50986e4d3c89ffbd
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-05-20 16:07:22 +0200

    all basic features added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+  name = Faker::Restaurant.name + Faker::Marketing.buzzwords
+  address = Faker::Address.city + Faker::Address.street_address
+  description = Faker::Restaurant.description 
+  price_per_night = rand(15..300)
+  number_of_guests = rand(1..10)
+
+  new_flat = Flat.new(
+    name: name,
+    address: address,
+    description: description,
+    price_per_night: price_per_night,
+    number_of_guests: number_of_guests
+    )
+  new_flat.save!
+end
